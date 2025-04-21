fe2af00f3d82d8846b6073a2f31f49e5fbb5829c
IvonJIAO <ivonjiao@gmail.com>
2025-04-21 21:33:55 +0800

    lab3

---
+++ LAB_25spring/lab3/lab3/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/SEP_file/SJTU_SEP_LAB/LAB_25spring/lab3/lab3
+# Build directory: C:/SEP_file/SJTU_SEP_LAB/LAB_25spring/lab3/lab3/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/SEP_file/SJTU_SEP_LAB/LAB_25spring/lab3/lab3/cmake-build-debug/lab3_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
---
+++ LAB_25spring/lab3/lab3/cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/SEP_file/SJTU_SEP_LAB/LAB_25spring/lab3/lab3/cmake-build-debug/_deps/googletest-src
+# Build directory: C:/SEP_file/SJTU_SEP_LAB/LAB_25spring/lab3/lab3/cmake-build-debug/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
